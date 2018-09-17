ce04d94aaf3739aa3d6d2325ab616719f9d8af39
Miguel Ángel Durán <miduga@gmail.com>
2018-09-17 15:06:39 +0200

    feat(cmp/modal): stop waiting cmp library for using it

--- components/cmp/modal/src/index.js
+++ components/cmp/modal/src/index.js
@@ -2,24 +2,20 @@ import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 
 import {CmpModalContainer} from './CmpModal'
-import CmpServices, {CmpWaitForLibrary} from '@s-ui/react-cmp-services'
+import CmpServices from '@s-ui/react-cmp-services'
 
 class CmpModal extends Component {
   render() {
-    const {checkCmpLibraryIsLoaded} = this.props
-
     return (
-      <CmpWaitForLibrary cmpReady={!checkCmpLibraryIsLoaded}>
-        <CmpServices>
-          {({getPurposesAndVendors, sendConsents}) => (
-            <CmpModalContainer
-              {...this.props}
-              getPurposesAndVendors={getPurposesAndVendors}
-              sendConsents={sendConsents}
-            />
-          )}
-        </CmpServices>
-      </CmpWaitForLibrary>
+      <CmpServices>
+        {({getPurposesAndVendors, sendConsents}) => (
+          <CmpModalContainer
+            {...this.props}
+            getPurposesAndVendors={getPurposesAndVendors}
+            sendConsents={sendConsents}
+          />
+        )}
+      </CmpServices>
     )
   }
 }
