6e07616c16f0b3d5a9a5464557fced5d5d0e0f31
Miguel Ángel Durán <miduga@gmail.com>
2018-10-02 12:53:45 +0200

    feat(cmp/modal): Add italian language

--- components/cmp/modal/src/settings.js
+++ components/cmp/modal/src/settings.js
@@ -37,10 +37,50 @@ Ten en cuenta que estas cookies van ligadas a tu sesión en el navegador por lo 
   TITLE: 'Tu privacidad es importante para nosotros'
 }
 
+const it = {
+  ADVERTISEMENT_TITLE:
+    'Cookie di prima o terza parte per la pubblicità segmentata',
+  ADVERTISEMENT_BODY: `Ci preoccupiamo molto della tua privacy, per questo motivo vogliamo
+  informarti delle finalità perseguite dai cookie per la pubblicità personalizzata, e farti sapere con chi
+  stiamo condividendo le tue informazioni. Potrai inoltre definire le finalità e le terze parti con le quali
+  accetti o meno di condividere i tuoi dati di navigazione, di posizione e i tuoi dati personali.
+
+  Tieni presente che questi cookie sono legati alla tua sessione di navigazione, quindi se aggiorni i
+  tuoi cookie, cambiando dispositivo o connettendoti da un altro browser, dovrai riconfigurare le tue
+  preferenze.`,
+  ACCEPT: 'Accetto',
+  AUTHORIZE: 'Autorizzo',
+  BACK: 'Indietro',
+  BODY: `Sia noi che i nostri partner utilizziamo cookie sul nostro sito web per fornire il servizio richiesto,
+    sviluppare statistiche, promuovere il traffico sul web e mostrarti pubblicità personalizzata sulla
+    base dei tuoi dati di navigazione o sviluppare profili pubblicitari in funzione dei dati che raccogliamo
+    su di te, che possiamo condividere con terze parti. Cliccando, acconsenti all’utilizzo di questa
+    tecnologia sul nostro sito web. Puoi cambiare idea e personalizzare il tuo consenso ogni volta che
+    vuoi tornando su questa schermata di configurazione.`,
+  COOKIES_SESSION: 'Cookie di sessione',
+  COOKIES_SESSION_DESC:
+    'I cookie sono indispensabili affinché il nostro sito web funzioni al meglio. Archiviamo le informazioni di navigazione e le preferenze di configurazione.',
+  COOKIES_ANALYTICS: 'Cookie analytics',
+  COOKIES_ANALYTICS_DESC:
+    'I cookie di prima o terza parte per misurare l’attività sul sito e per conoscere l’utilizzo fatto dagli utenti dei nostri servizi e poterli così migliorare. Memorizzano le informazioni di navigazione all’interno del nostro sito web come informazioni circa l’origine del traffico, informazioni di navigazione e preferenze di configurazione.',
+  COOKIES_MARKETING: 'Cookie marketing',
+  COOKIES_MARKETING_DESC:
+    'I cookie di prima o terza parte per realizzare campagne marketing, per portare e trattenere il traffico sul nostro sito web. Memorizzano le informazioni di navigazione all’interno o all’esterno del nostro sito web.',
+  COOKIES_ADS: 'Cookie pubblicitari',
+  COOKIES_ADS_DESC:
+    'I cookie di prima o terza parte per offrire ai nostri partner pubblicitari un’audience segmentata. Memorizzano le informazioni di navigazione all’interno o all’esterno del nostro sito web, così come la tua posizione o i dati personali.',
+  DISABLE_ALL: 'Disabilita tutto',
+  ENABLE_ALL: 'Abilita tutto',
+  FOR_THE_NEXT_PARTNERS: 'Per i prossimi inserzionisti e partner',
+  PERSONALIZE: 'Personalizza',
+  READ_MORE: 'Leggi di più',
+  TITLE: 'La tua privacy è importante per noi'
+}
+
 export const I18N = {
+  it,
   es: DEFAULT_I18N,
   en: DEFAULT_I18N,
   ca: DEFAULT_I18N,
-  de: DEFAULT_I18N,
-  it: DEFAULT_I18N
+  de: DEFAULT_I18N
 }
