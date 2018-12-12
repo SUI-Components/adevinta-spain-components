8311d1ed15506f267ecabb5c02db8ee6a0721797
AitorRodriguez990 <aitor.rodriguez990@gmail.com>
2018-12-12 08:21:09 +0100

    feat(form/checkboxesWithParent): new component to show a list of checkboxes with a parent checkbox

---
+++ components/form/checkboxesWithParent/README.md
@@ -0,0 +1,23 @@
+# FormCheckboxesWithParent
+
+> Description
+
+<!-- ![](./assets/preview.png) -->
+
+## Installation
+
+```sh
+$ npm install @schibstedspain/sui-form-checkboxes-with-parent --save
+```
+
+## Usage
+
+### Basic usage
+```js
+import FormCheckboxesWithParent from '@schibstedspain/sui-form-checkboxes-with-parent'
+
+return (<FormCheckboxesWithParent />)
+```
+
+
+> **Find full description and more examples in the [demo page](#).**
---
+++ components/form/checkboxesWithParent/src/index.js
@@ -0,0 +1,82 @@
+import React, {PureComponent} from 'react'
+import FormCheckbox from '@schibstedspain/sui-form-checkbox'
+import PropTypes from 'prop-types'
+
+const BASE_CLASS = 'sui-FormCheckboxesWithParent'
+
+class FormCheckboxesWithParent extends PureComponent {
+  state = {
+    childrenCheckbox: [...this.props.initialChildrenCheckbox]
+  }
+
+  _createHandleChangeParent = checked => () => {
+    const newChildrenCheckbox = this.state.childrenCheckbox.map(checkbox => {
+      return {...checkbox, checked: !checked}
+    })
+
+    this.setState({childrenCheckbox: newChildrenCheckbox})
+  }
+
+  _createHandleChangeChildren = index => () => {
+    const {childrenCheckbox} = this.state
+
+    const newChildrenCheckbox = childrenCheckbox.map(
+      (checkbox, checkboxIndex) => {
+        if (checkboxIndex === index) {
+          return {...checkbox, checked: !checkbox.checked}
+        }
+        return checkbox
+      }
+    )
+
+    this.setState({childrenCheckbox: newChildrenCheckbox})
+  }
+
+  componentDidUpdate() {
+    this.props.handleUpdateCheckboxes(this.state.childrenCheckbox)
+  }
+
+  render() {
+    const {parentCheckbox} = this.props
+    const {childrenCheckbox} = this.state
+
+    const checked = childrenCheckbox.every(checkbox => checkbox.checked)
+
+    return (
+      <div className={BASE_CLASS}>
+        <FormCheckbox
+          name={parentCheckbox.name}
+          checked={checked}
+          onChange={this._createHandleChangeParent(checked)}
+          label={parentCheckbox.label}
+        />
+
+        <div className={`${BASE_CLASS}-children`}>
+          {childrenCheckbox.map((checkbox, index) => (
+            <FormCheckbox
+              name={checkbox.name}
+              checked={checkbox.checked}
+              onChange={this._createHandleChangeChildren(index)}
+              label={checkbox.label}
+              key={index}
+            />
+          ))}
+        </div>
+      </div>
+    )
+  }
+}
+
+FormCheckboxesWithParent.displayName = 'FormCheckboxesWithParent'
+
+FormCheckboxesWithParent.defaultProps = {
+  handleUpdateCheckboxes: () => {}
+}
+
+FormCheckboxesWithParent.propTypes = {
+  parentCheckbox: PropTypes.object,
+  initialChildrenCheckbox: PropTypes.array,
+  handleUpdateCheckboxes: PropTypes.func
+}
+
+export default FormCheckboxesWithParent
