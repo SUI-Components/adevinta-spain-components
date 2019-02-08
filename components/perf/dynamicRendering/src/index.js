6c14185d05d1ea40cde4472cb39207d16ab3b3ef
Carlos Villuendas <carlosvillu@gmail.com>
2019-02-08 11:52:48 +0100

    feat(perf/dynamicRendering): add force prop to force render the children always in server and client

--- components/perf/dynamicRendering/src/index.js
+++ components/perf/dynamicRendering/src/index.js
@@ -33,18 +33,22 @@ function checkUserAgentIsBot({userAgent}) {
 export default function PerfDynamicRendering({
   children,
   disabled,
+  force,
   height,
   userAgent
 }) {
   const isBot = checkUserAgentIsBot({userAgent})
   const isOnBrowser = typeof window !== 'undefined'
 
+  // Force render in server and client
+  if (force) return children
+
   // if isBot or disabled, we return in server and client the content
-  if (isBot || disabled) return children
+  if (isBot) return children
 
   // now, we're sure the user isNotBot
   // so check if we're on the browser side and if is not disabled the component
-  if (isOnBrowser) {
+  if (isOnBrowser && !disabled) {
     return <LazyContent height={height}>{children}</LazyContent>
   } else {
     // so, we're on the server side or the component is disabled
@@ -59,14 +63,22 @@ PerfDynamicRendering.propTypes = {
    * Wrapped content that will be rendered, or not, depending your userAgent and if is interescting. As explained in the README.md
    */
   children: PropTypes.any,
+
   /**
-   * Flag that determines if you don't want to render anything in the client. ⚠️ This prop doesn't apply if the userAgent is a bot.
+   * Flag that determines if you want disable the Intersection Observer lazy load strategy. And use a controller lazy strategy ONLY in client side.
    */
   disabled: PropTypes.bool,
+
+  /**
+   * avoid any check and render always.
+   * */
+  force: PropTypes.bool,
+
   /**
    * Number that determines the height of the component that we're waiting in pixels.
    */
   height: PropTypes.number,
+
   /**
    * String with the userAgent that will be used to check if is bot or normal user.
    */
