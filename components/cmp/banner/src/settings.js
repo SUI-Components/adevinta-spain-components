7ee28c9faf1438e2136643191df89aadf6a2d4e7
Ferran Simon <ferran.simon@AFE0811293.local>
2019-04-02 09:45:40 +0200

    feat(cmp/banner): added italy literals

--- components/cmp/banner/src/settings.js
+++ components/cmp/banner/src/settings.js
@@ -10,7 +10,7 @@ const it = {
   ACCEPT_BUTTON: 'Continua la navigazione',
   READ_MORE_BUTTON: 'Modifica impostazioni',
   BANNER_BODY:
-    'In %{companyName} utilizziamo i cookie di prima o terza parte per fornire il servizio richiesto, sviluppare statistiche, promuovere il traffico sul web e mostrarti pubblicità personalizzata sulla base dei tuoi dati di navigazione o sviluppare profili pubblicitari in funzione dei dati che raccogliamo su di te, che possiamo condividere con terze parti. Se continui a navigare ne accetti l’utilizzo. È possibile configurare o rifiutare i cookie facendo clic su ‘modifica impostazioni'
+    'Utilizziamo i cookie di prima o terza parte per fornire il servizio richiesto, sviluppare statistiche, promuovere il traffico sul web e mostrarti pubblicità personalizzata sulla base dei tuoi dati di navigazione o sviluppare profili pubblicitari in funzione dei dati che raccogliamo su di te, che possiamo condividere con terze parti. Se continui a navigare ne accetti l’utilizzo. È\' possibile configurare o rifiutare i cookie facendo clic su <a class="sui-CmpBanner-link" href="#">politica sui cookie.</a>'
 }
 
 export const I18N = {
