62c266dab3449c3af36abbda7f8ed4d87cd39d05
midudev <miduga@gmail.com>
2019-06-19 11:35:49 +0200

    feat(cmp/banner): Move to functions

--- components/cmp/banner/src/CmpBanner/index.js
+++ components/cmp/banner/src/CmpBanner/index.js
@@ -1,5 +1,6 @@
-import React, {Component, Suspense} from 'react'
+import React, {useState, useRef, Suspense} from 'react'
 import PropTypes from 'prop-types'
+import {useMount} from '@schibstedspain/sui-react-hooks'
 
 const CONSENT_STATUS_NOT_ACCEPTED = 'NOT_ACCEPTED'
 const NO_OP = () => {}
@@ -7,29 +8,39 @@ const NO_OP = () => {}
 const CmpBanner = React.lazy(() => import('./component'))
 const CmpModal = React.lazy(() => import('@schibstedspain/react-cmp-modal'))
 
-export class CmpBannerContainer extends Component {
-  state = {
-    showModal: false,
-    showNotification: false
-  }
+export default function CmpBannerContainer({
+  companyName,
+  lang,
+  logo,
+  privacyUrl,
+  getConsentStatus,
+  getPurposesAndVendors,
+  sendConsents,
+  onAccept = NO_OP,
+  onConfigure = NO_OP
+}) {
+  const [showModal, setShowModal] = useState(false)
+  const [showNotification, setShowNotification] = useState(false)
+  const containerDOMEl = useRef()
 
-  containerDOMEl = React.createRef()
-
-  _handleAccept = async () => {
-    const {getPurposesAndVendors, sendConsents} = this.props
+  const _handleAccept = async () => {
     const {
       purposeConsents,
       vendorConsents
     } = await getPurposesAndVendors.execute()
     await sendConsents.execute({purposeConsents, vendorConsents})
-    this.setState({showModal: false, showNotification: false})
-    this._removeBodyEvent()
-    this.props.onAccept()
+
+    setShowModal(false)
+    setShowNotification(false)
+
+    _removeBodyEvent()
+    onAccept()
   }
 
-  _handleReadMore = async () => {
-    this.setState({showModal: true, showNotification: false})
-    this.props.onConfigure()
+  const _handleReadMore = async () => {
+    setShowModal(true)
+    setShowNotification(true)
+    onConfigure()
   }
 
   /**
@@ -37,72 +48,63 @@ export class CmpBannerContainer extends Component {
    * in order to assume he's accepting our CMP if he's not trying to configure it and
    * he's just navigating the website
    */
-  _handleClickOnDocument = ({target}) => {
+  const _handleClickOnDocument = ({target}) => {
     const isClickableElement =
       target.closest('a') || target.closest('button') || target.closest('input')
-    const isNotContainedInBanner = !this.containerDOMEl.current.contains(target)
+    const isNotContainedInBanner = !containerDOMEl.current.contains(target)
     // if not contained and clickable element, then accept the banner
-    isNotContainedInBanner && isClickableElement && this._handleAccept()
+    isNotContainedInBanner && isClickableElement && _handleAccept()
   }
 
-  _handleExitModal = () => {
-    this.setState({showModal: false})
+  const _handleExitModal = () => {
+    setShowModal(false)
   }
 
-  _removeBodyEvent() {
-    document.removeEventListener('click', this._handleClickOnDocument, true)
+  const _removeBodyEvent = () => {
+    document.removeEventListener('click', _handleClickOnDocument)
   }
 
-  async componentDidMount() {
-    const {getConsentStatus} = this.props
-    const consentStatus = await getConsentStatus.execute()
-    this.setState({
-      showNotification: consentStatus === CONSENT_STATUS_NOT_ACCEPTED
-    })
+  useMount(function() {
+    async function initiConsentStatus() {
+      const consentStatus = await getConsentStatus.execute()
+      const isConsentNotAccepted = consentStatus === CONSENT_STATUS_NOT_ACCEPTED
+      setShowNotification(isConsentNotAccepted)
 
-    if (consentStatus === CONSENT_STATUS_NOT_ACCEPTED) {
-      // We're assuming, the user accepts our CMP if he keep navigating in our page
-      document.addEventListener('click', this._handleClickOnDocument, true)
+      if (isConsentNotAccepted) {
+        // We're assuming, the user accepts our CMP if he keep navigating in our page
+        document.addEventListener('click', _handleClickOnDocument)
+      }
     }
-  }
+    initiConsentStatus()
 
-  componentWillUnmount() {
-    this._removeBodyEvent()
-  }
+    return _removeBodyEvent
+  })
 
-  render() {
-    const {companyName, lang, logo, privacyUrl} = this.props
-    return (
-      <div ref={this.containerDOMEl}>
-        {this.state.showNotification && (
-          <Suspense fallback={<div />}>
-            <CmpBanner
-              onAccept={this._handleAccept}
-              onConfigure={this._handleReadMore}
-              companyName={companyName}
-              lang={lang}
-            />
-          </Suspense>
-        )}
-        {this.state.showModal && (
-          <Suspense fallback={<div />}>
-            <CmpModal
-              cmpReady
-              lang={lang}
-              logo={logo}
-              onExit={this._handleExitModal}
-              privacyUrl={privacyUrl}
-            />
-          </Suspense>
-        )}
-      </div>
-    )
-  }
-}
-
-CmpBannerContainer.defaultProps = {
-  onAccept: NO_OP,
-  onConfigure: NO_OP
+  return (
+    <div ref={containerDOMEl}>
+      {showNotification && (
+        <Suspense fallback={<div />}>
+          <CmpBanner
+            onAccept={_handleAccept}
+            onConfigure={_handleReadMore}
+            companyName={companyName}
+            lang={lang}
+          />
+        </Suspense>
+      )}
+      {showModal && (
+        <Suspense fallback={<div />}>
+          <CmpModal
+            cmpReady
+            lang={lang}
+            logo={logo}
+            onExit={_handleExitModal}
+            privacyUrl={privacyUrl}
+          />
+        </Suspense>
+      )}
+    </div>
+  )
 }
 
 CmpBannerContainer.propTypes = {
