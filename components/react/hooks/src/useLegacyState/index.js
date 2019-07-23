3467709ad4bebf6e1696a9d23f47b2e479a9226d
desko27 <desko27@gmail.com>
2019-07-23 16:24:19 +0200

    fix(react/hooks): make sure legacy state hook extends the most updated state

--- components/react/hooks/src/useLegacyState/index.js
+++ components/react/hooks/src/useLegacyState/index.js
@@ -2,6 +2,7 @@ import {useState} from 'react'
 
 export default function useLegacyState(initialState = {}) {
   const [state, setState] = useState(initialState)
-  const setLegacyState = newState => setState({...state, ...newState})
+  const setLegacyState = newState =>
+    setState(prevState => ({...prevState, ...newState}))
   return [state, setLegacyState]
 }
