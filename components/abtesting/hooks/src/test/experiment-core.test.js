bab7b2ce7e1a0d925da65314042e96268b03c73b
desko27 <desko27@gmail.com>
2019-08-01 12:27:37 +0200

    test(abtesting/hooks): reuse asserting logic via assertActivation function

--- components/abtesting/hooks/src/test/experiment-core.test.js
+++ components/abtesting/hooks/src/test/experiment-core.test.js
@@ -16,53 +16,42 @@ Enzyme.configure({adapter: new Adapter()})
 jest.useFakeTimers()
 
 // configure dependencies
-const useExperiment = () => useExperimentRaw({ExperimentContext})
+const useExperiment = params => useExperimentRaw({ExperimentContext, ...params})
 const OptimizelyXExperiment = props => (
   <AbTestOptimizelyXExperiment {...props} deps={{useExperimentCore}} />
 )
 
-describe('useExperiment', () => {
-  describe('when the component IS NOT wrapped by an experiment component', () => {
-    it('should output isWrapped to false', () => {
-      const Child = () => {
-        const {isWrapped} = useExperiment()
-        return isWrapped.toString()
-      }
-      const Component = () => (
-        <div>
-          <Child />
-        </div>
-      )
+describe('Experiment', () => {
+  let activationHandler
 
-      let mounted
-      act(() => {
-        mounted = mount(<Component />)
-      })
-      expect(mounted.html()).toEqual('<div>false</div>')
+  const assertActivation = (
+    Component,
+    expectedFromDefault,
+    variationId,
+    expectedFromVariation
+  ) => {
+    let mounted
+    act(() => {
+      mounted = mount(<Component />)
     })
+    expect(mounted.html()).toEqual(expectedFromDefault)
 
-    it('should output isDefault to true', () => {
-      const Child = () => {
-        const {isDefault} = useExperiment()
-        return isDefault.toString()
+    if (variationId) {
+      if (variationId === 'runAllTimers') {
+        act(() => {
+          jest.runAllTimers()
+        })
+      } else {
+        act(() => {
+          activationHandler(variationId)
+        })
       }
-      const Component = () => (
-        <div>
-          <Child />
-        </div>
-      )
+      mounted.update()
+      expect(mounted.html()).toEqual(expectedFromVariation)
+    }
+  }
 
-      let mounted
-      act(() => {
-        mounted = mount(<Component />)
-      })
-      expect(mounted.html()).toEqual('<div>true</div>')
-    })
-  })
-
-  describe('when the component IS wrapped by an experiment component', () => {
-    let activationHandler
-
+  describe('from useExperiment acting as an CORE RUNNER', () => {
     beforeEach(() => {
       jest
         .spyOn(OptimizelyX, 'addActivationListener')
@@ -76,294 +65,286 @@ describe('useExperiment', () => {
     })
 
     it('should output isWrapped to true, then keep isWrapped to true', () => {
-      const Child = () => {
-        const {isWrapped} = useExperiment()
-        return isWrapped.toString()
-      }
-      const Component = () => (
-        <OptimizelyXExperiment experimentId={40000}>
-          <Child defaultVariation variationId={700000} />
-          <Child variationId={700001} />
-          <Child variationId={700002} />
-        </OptimizelyXExperiment>
+      assertActivation(
+        () => {
+          const experimentData = useExperiment({
+            experimentId: 40000,
+            variations: [
+              {id: 700000, isDefault: true},
+              {id: 700001},
+              {id: 700002}
+            ]
+          })
+          const {isWrapped} = experimentData
+          return isWrapped.toString()
+        },
+        'true',
+        700001,
+        'true'
       )
+    })
+  })
 
-      let mounted
-      act(() => {
-        mounted = mount(<Component />)
+  describe('from useExperiment acting as a CONTEXT CONSUMER and OptimizelyXExperiment as a CORE RUNNER', () => {
+    describe('when the component IS NOT wrapped by an experiment component', () => {
+      it('should output isWrapped to false', () => {
+        const Child = () => {
+          const {isWrapped} = useExperiment()
+          return isWrapped.toString()
+        }
+        assertActivation(
+          () => (
+            <div>
+              <Child />
+            </div>
+          ),
+          '<div>false</div>'
+        )
       })
-      expect(mounted.html()).toEqual('true')
 
-      act(() => {
-        activationHandler(700001)
+      it('should output isDefault to true', () => {
+        const Child = () => {
+          const {isDefault} = useExperiment()
+          return isDefault.toString()
+        }
+        assertActivation(
+          () => (
+            <div>
+              <Child />
+            </div>
+          ),
+          '<div>true</div>'
+        )
       })
-      expect(mounted.html()).toEqual('true')
     })
 
-    it('should output isActive to false, then set isActive to true', () => {
-      const Child = () => {
-        const {isActive} = useExperiment()
-        return isActive.toString()
-      }
-      const Component = () => (
-        <OptimizelyXExperiment experimentId={40000}>
-          <Child defaultVariation variationId={700000} />
-          <Child variationId={700001} />
-          <Child variationId={700002} />
-        </OptimizelyXExperiment>
-      )
-
-      let mounted
-      act(() => {
-        mounted = mount(<Component />)
+    describe('when the component IS wrapped by an experiment component', () => {
+      beforeEach(() => {
+        jest
+          .spyOn(OptimizelyX, 'addActivationListener')
+          .mockImplementation((experimentId, handler) => {
+            activationHandler = handler
+          })
       })
-      expect(mounted.html()).toEqual('false')
 
-      act(() => {
-        activationHandler(700001)
+      afterEach(() => {
+        OptimizelyX.addActivationListener.mockRestore()
       })
-      mounted.update()
-      expect(mounted.html()).toEqual('true')
-    })
 
-    it('should output isDefault to true, then set isDefault to false', () => {
-      const Child = () => {
-        const {isDefault} = useExperiment()
-        return isDefault.toString()
-      }
-      const Component = () => (
-        <OptimizelyXExperiment experimentId={40000}>
-          <Child defaultVariation variationId={700000} />
-          <Child variationId={700001} />
-          <Child variationId={700002} />
-        </OptimizelyXExperiment>
-      )
-
-      let mounted
-      act(() => {
-        mounted = mount(<Component />)
+      it('should output isWrapped to true, then keep isWrapped to true', () => {
+        const Child = () => {
+          const {isWrapped} = useExperiment()
+          return isWrapped.toString()
+        }
+        assertActivation(
+          () => (
+            <OptimizelyXExperiment experimentId={40000}>
+              <Child defaultVariation variationId={700000} />
+              <Child variationId={700001} />
+              <Child variationId={700002} />
+            </OptimizelyXExperiment>
+          ),
+          'true',
+          700001,
+          'true'
+        )
       })
-      expect(mounted.html()).toEqual('true')
 
-      act(() => {
-        activationHandler(700001)
+      it('should output isActive to false, then set isActive to true', () => {
+        const Child = () => {
+          const {isActive} = useExperiment()
+          return isActive.toString()
+        }
+        assertActivation(
+          () => (
+            <OptimizelyXExperiment experimentId={40000}>
+              <Child defaultVariation variationId={700000} />
+              <Child variationId={700001} />
+              <Child variationId={700002} />
+            </OptimizelyXExperiment>
+          ),
+          'false',
+          700001,
+          'true'
+        )
       })
-      mounted.update()
-      expect(mounted.html()).toEqual('false')
-    })
 
-    it('should output isVariation to false, then set isVariation to true', () => {
-      const Child = () => {
-        const {isVariation} = useExperiment()
-        return isVariation.toString()
-      }
-      const Component = () => (
-        <OptimizelyXExperiment experimentId={40000}>
-          <Child defaultVariation variationId={700000} />
-          <Child variationId={700001} />
-          <Child variationId={700002} />
-        </OptimizelyXExperiment>
-      )
-
-      let mounted
-      act(() => {
-        mounted = mount(<Component />)
+      it('should output isDefault to true, then set isDefault to false', () => {
+        const Child = () => {
+          const {isDefault} = useExperiment()
+          return isDefault.toString()
+        }
+        assertActivation(
+          () => (
+            <OptimizelyXExperiment experimentId={40000}>
+              <Child defaultVariation variationId={700000} />
+              <Child variationId={700001} />
+              <Child variationId={700002} />
+            </OptimizelyXExperiment>
+          ),
+          'true',
+          700001,
+          'false'
+        )
       })
-      expect(mounted.html()).toEqual('false')
 
-      act(() => {
-        activationHandler(700001)
+      it('should output isVariation to false, then set isVariation to true', () => {
+        const Child = () => {
+          const {isVariation} = useExperiment()
+          return isVariation.toString()
+        }
+        assertActivation(
+          () => (
+            <OptimizelyXExperiment experimentId={40000}>
+              <Child defaultVariation variationId={700000} />
+              <Child variationId={700001} />
+              <Child variationId={700002} />
+            </OptimizelyXExperiment>
+          ),
+          'false',
+          700001,
+          'true'
+        )
       })
-      mounted.update()
-      expect(mounted.html()).toEqual('true')
-    })
 
-    it('should contain parent experimentId and choosen variationId, then keep experimentId and set variationId to the choosen one', () => {
-      const Child = () => {
-        const {experimentId, variationId} = useExperiment()
-        return `${experimentId}/${variationId}`
-      }
-      const Component = () => (
-        <OptimizelyXExperiment experimentId={40000}>
-          <Child defaultVariation variationId={700000} />
-          <Child variationId={700001} />
-          <Child variationId={700002} />
-        </OptimizelyXExperiment>
-      )
-
-      let mounted
-      act(() => {
-        mounted = mount(<Component />)
+      it('should contain parent experimentId and choosen variationId, then keep experimentId and set variationId to the choosen one', () => {
+        const Child = () => {
+          const {experimentId, variationId} = useExperiment()
+          return `${experimentId}/${variationId}`
+        }
+        assertActivation(
+          () => (
+            <OptimizelyXExperiment experimentId={40000}>
+              <Child defaultVariation variationId={700000} />
+              <Child variationId={700001} />
+              <Child variationId={700002} />
+            </OptimizelyXExperiment>
+          ),
+          '40000/700000',
+          700001,
+          '40000/700001'
+        )
       })
-      expect(mounted.html()).toEqual('40000/700000')
 
-      act(() => {
-        activationHandler(700001)
+      it('should match default variation in variationName, then match the choosen variation in variationName', () => {
+        const Child = () => {
+          const {variationName} = useExperiment()
+          return variationName
+        }
+        assertActivation(
+          () => (
+            <OptimizelyXExperiment experimentId={40000}>
+              <Child variationId={700000} />
+              <Child variationId={700001} />
+              <Child defaultVariation variationId={700002} />
+            </OptimizelyXExperiment>
+          ),
+          'C',
+          700001,
+          'B'
+        )
       })
-      mounted.update()
-      expect(mounted.html()).toEqual('40000/700001')
-    })
 
-    it('should match default variation in variationName, then match the choosen variation in variationName', () => {
-      const Child = () => {
-        const {variationName} = useExperiment()
-        return variationName
-      }
-      const Component = () => (
-        <OptimizelyXExperiment experimentId={40000}>
-          <Child variationId={700000} />
-          <Child variationId={700001} />
-          <Child defaultVariation variationId={700002} />
-        </OptimizelyXExperiment>
-      )
-
-      let mounted
-      act(() => {
-        mounted = mount(<Component />)
+      it('should match default variation in variation flags, then match the choosen variation in variation flags', () => {
+        const Child = () => {
+          const {isVariationA, isVariationB, isVariationC} = useExperiment()
+          return `${isVariationA}:${isVariationB}:${isVariationC}`
+        }
+        assertActivation(
+          () => (
+            <OptimizelyXExperiment experimentId={40000}>
+              <Child variationId={700000} />
+              <Child variationId={700001} />
+              <Child defaultVariation variationId={700002} />
+            </OptimizelyXExperiment>
+          ),
+          'false:false:true',
+          700000,
+          'true:false:false'
+        )
       })
-      expect(mounted.html()).toEqual('C')
 
-      act(() => {
-        activationHandler(700001)
+      it('should always keep forceVariation when provided as an id', () => {
+        const Child = () => {
+          const {variationName} = useExperiment()
+          return variationName
+        }
+        assertActivation(
+          () => (
+            <OptimizelyXExperiment
+              experimentId={40000}
+              forceVariation={700001} // B
+            >
+              <Child defaultVariation variationId={700000} />
+              <Child variationId={700001} />
+              <Child variationId={700002} />
+            </OptimizelyXExperiment>
+          ),
+          'B',
+          700002,
+          'B'
+        )
       })
-      mounted.update()
-      expect(mounted.html()).toEqual('B')
-    })
 
-    it('should match default variation in variation flags, then match the choosen variation in variation flags', () => {
-      const Child = () => {
-        const {isVariationA, isVariationB, isVariationC} = useExperiment()
-        return `${isVariationA}:${isVariationB}:${isVariationC}`
-      }
-      const Component = () => (
-        <OptimizelyXExperiment experimentId={40000}>
-          <Child variationId={700000} />
-          <Child variationId={700001} />
-          <Child defaultVariation variationId={700002} />
-        </OptimizelyXExperiment>
-      )
-
-      let mounted
-      act(() => {
-        mounted = mount(<Component />)
+      it('should always keep forceVariation when provided as a name', () => {
+        const Child = () => {
+          const {variationName} = useExperiment()
+          return variationName
+        }
+        assertActivation(
+          () => (
+            <OptimizelyXExperiment experimentId={40000} forceVariation="B">
+              <Child defaultVariation variationId={700000} />
+              <Child variationId={700001} />
+              <Child variationId={700002} />
+            </OptimizelyXExperiment>
+          ),
+          'B',
+          700002,
+          'B'
+        )
       })
-      expect(mounted.html()).toEqual('false:false:true')
 
-      act(() => {
-        activationHandler(700000)
+      it('should display default variation first, then display forceActivation after a few milliseconds when provided as an id', () => {
+        const Child = () => {
+          const {variationName} = useExperiment()
+          return variationName
+        }
+        assertActivation(
+          () => (
+            <OptimizelyXExperiment
+              experimentId={40000}
+              forceActivation={700001} // B
+            >
+              <Child defaultVariation variationId={700000} />
+              <Child variationId={700001} />
+              <Child variationId={700002} />
+            </OptimizelyXExperiment>
+          ),
+          'A',
+          'runAllTimers',
+          'B'
+        )
       })
-      mounted.update()
-      expect(mounted.html()).toEqual('true:false:false')
-    })
 
-    it('should always keep forceVariation when provided as an id', () => {
-      const Child = () => {
-        const {variationName} = useExperiment()
-        return variationName
-      }
-      const Component = () => (
-        <OptimizelyXExperiment
-          experimentId={40000}
-          forceVariation={700001} // B
-        >
-          <Child defaultVariation variationId={700000} />
-          <Child variationId={700001} />
-          <Child variationId={700002} />
-        </OptimizelyXExperiment>
-      )
-
-      let mounted
-      act(() => {
-        mounted = mount(<Component />)
+      it('should display default variation first, then display forceActivation after a few milliseconds when provided as a name', () => {
+        const Child = () => {
+          const {variationName} = useExperiment()
+          return variationName
+        }
+        assertActivation(
+          () => (
+            <OptimizelyXExperiment experimentId={40000} forceActivation="B">
+              <Child defaultVariation variationId={700000} />
+              <Child variationId={700001} />
+              <Child variationId={700002} />
+            </OptimizelyXExperiment>
+          ),
+          'A',
+          'runAllTimers',
+          'B'
+        )
       })
-      expect(mounted.html()).toEqual('B')
-
-      act(() => {
-        activationHandler(700002)
-      })
-      mounted.update()
-      expect(mounted.html()).toEqual('B')
-    })
-
-    it('should always keep forceVariation when provided as a name', () => {
-      const Child = () => {
-        const {variationName} = useExperiment()
-        return variationName
-      }
-      const Component = () => (
-        <OptimizelyXExperiment experimentId={40000} forceVariation="B">
-          <Child defaultVariation variationId={700000} />
-          <Child variationId={700001} />
-          <Child variationId={700002} />
-        </OptimizelyXExperiment>
-      )
-
-      let mounted
-      act(() => {
-        mounted = mount(<Component />)
-      })
-      expect(mounted.html()).toEqual('B')
-
-      act(() => {
-        activationHandler(700002)
-      })
-      mounted.update()
-      expect(mounted.html()).toEqual('B')
-    })
-
-    it('should display default variation first, then display forceActivation after a few milliseconds when provided as an id', () => {
-      const Child = () => {
-        const {variationName} = useExperiment()
-        return variationName
-      }
-      const Component = () => (
-        <OptimizelyXExperiment
-          experimentId={40000}
-          forceActivation={700001} // B
-        >
-          <Child defaultVariation variationId={700000} />
-          <Child variationId={700001} />
-          <Child variationId={700002} />
-        </OptimizelyXExperiment>
-      )
-
-      let mounted
-      act(() => {
-        mounted = mount(<Component />)
-      })
-      expect(mounted.html()).toEqual('A')
-
-      act(() => {
-        jest.runAllTimers()
-      })
-      mounted.update()
-      expect(mounted.html()).toEqual('B')
-    })
-
-    it('should display default variation first, then display forceActivation after a few milliseconds when provided as a name', () => {
-      const Child = () => {
-        const {variationName} = useExperiment()
-        return variationName
-      }
-      const Component = () => (
-        <OptimizelyXExperiment experimentId={40000} forceActivation="B">
-          <Child defaultVariation variationId={700000} />
-          <Child variationId={700001} />
-          <Child variationId={700002} />
-        </OptimizelyXExperiment>
-      )
-
-      let mounted
-      act(() => {
-        mounted = mount(<Component />)
-      })
-      expect(mounted.html()).toEqual('A')
-
-      act(() => {
-        jest.runAllTimers()
-      })
-      mounted.update()
-      expect(mounted.html()).toEqual('B')
     })
   })
 })
