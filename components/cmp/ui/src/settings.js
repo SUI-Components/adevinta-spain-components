db1ed60891c147824467f82c6a5683bab4dc0883
midudev <miduga@gmail.com>
2019-08-01 13:28:55 +0200

    feat(cmp/ui): create Cmp/Ui component

---
+++ components/cmp/ui/src/settings.js
@@ -0,0 +1,29 @@
+export const CLASS = 'sui-CmpUi'
+export const UI_TYPES = {
+  BANNER: 'banner',
+  MODAL: 'modal'
+}
+
+
+const DEFAULT_I18N = {
+  ACCEPT_BUTTON: 'Seguir navegando',
+  BANNER_BODY:
+    'Utilizamos cookies propias y de terceros para recordar tus preferencias, elaborar estadísticas, crear perfiles publicitarios y mostrarte publicidad personalizada analizando tu navegación. Si continúas navegando o haces clic en el botón "Seguir navegando", aceptas su uso. Puedes revocar el consentimiento en <a class="sui-CmpUi-link" href="#">nuestra política de cookies</a>.',
+  TITLE: 'Tu privacidad es importante',
+  PARTNERS_LINK: 'Ver lista completa de partners'
+}
+
+const it = {
+  ACCEPT_BUTTON: 'Continua la navigazione',
+  READ_MORE_BUTTON: 'Modifica impostazioni',
+  BANNER_BODY:
+    'Utilizziamo i cookie di prima o terza parte per fornire il servizio richiesto, sviluppare statistiche, promuovere il traffico sul web e mostrarti pubblicità personalizzata sulla base dei tuoi dati di navigazione o sviluppare profili pubblicitari in funzione dei dati che raccogliamo su di te, che possiamo condividere con terze parti. Se continui a navigare ne accetti l’utilizzo. È\' possibile configurare o rifiutare i cookie facendo clic su <a class="sui-CmpUi-link" href="#">politica sui cookie.</a>'
+}
+
+export const I18N = {
+  it,
+  es: DEFAULT_I18N,
+  en: DEFAULT_I18N,
+  ca: DEFAULT_I18N,
+  de: DEFAULT_I18N
+}
