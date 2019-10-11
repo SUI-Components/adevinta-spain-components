f3782687777bbc233579508d7e0a37afbc16cb7b
Giorgio de la Barrera <giorgio.barrera@schibsted.com>
2019-10-11 13:48:56 +0200

    feat(react/hooks): add temporary solution for add compatibility

--- components/react/hooks/src/useMount/index.js
+++ components/react/hooks/src/useMount/index.js
@@ -1,5 +1,12 @@
 import {useEffect} from 'react'
 
 export default function useMount(effect) {
-  useEffect(effect, [])
+  useEffect(() => {
+    let clean = () => {}
+    async function doEffect() {
+      clean = await effect()
+    }
+    doEffect()
+    return () => clean()
+  }, []) // eslint-disable-line
 }
