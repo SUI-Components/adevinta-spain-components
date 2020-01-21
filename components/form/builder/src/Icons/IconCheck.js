5dceede78e3468aa7b1f295421e5ce8a39a84b2b
Carlos Villuendas <carlosvillu@gmail.com>
2020-01-21 14:53:40 +0100

    feat(form/builder): from compilate code to src code

--- components/form/builder/src/Icons/IconCheck.js
+++ components/form/builder/src/Icons/IconCheck.js
@@ -1,35 +1,14 @@
-import React, {memo, useEffect, useState} from 'react'
+import React, {memo} from 'react'
+import PropTypes from 'prop-types'
 
-var SVGComponent = function SVGComponent(_ref) {
-  var _ref$ssr = _ref.ssr
-  var ssr = _ref$ssr === void 0 ? false : _ref$ssr // eslint-disable-line
-  var className = _ref.className
+const SVG = ({className}) => (
+  <svg className={className} viewBox="0 0 24 24">
+    <path d="M18.5 6c.4-.4 1-.4 1.4 0s.4 1 0 1.4L9.7 17.7c-.4.4-1 .4-1.4 0l-4.5-4.5c-.4-.4-.4-1 0-1.4.4-.4 1-.4 1.4 0L9 15.6 18.5 6z" />
+  </svg>
+)
 
-  var _useState = useState(ssr)
-  var render = _useState[0]
-  var setRender = _useState[1]
-
-  useEffect(
-    function() {
-      if (render === false) {
-        setRender(true)
-      }
-    },
-    [render, setRender]
-  )
-  return render === false
-    ? null
-    : React.createElement(
-        'svg',
-        {
-          className: className,
-          viewBox: '0 0 24 24'
-        },
-        React.createElement('path', {
-          d:
-            'M18.5 6c.4-.4 1-.4 1.4 0s.4 1 0 1.4L9.7 17.7c-.4.4-1 .4-1.4 0l-4.5-4.5c-.4-.4-.4-1 0-1.4.4-.4 1-.4 1.4 0L9 15.6 18.5 6z'
-        })
-      )
+SVG.propTypes = {
+  className: PropTypes.string
 }
 
-export default memo(SVGComponent)
+export default memo(SVG)
--- components/form/builder/src/Icons/IconChevronDown.js
+++ components/form/builder/src/Icons/IconChevronDown.js
@@ -1,35 +1,14 @@
-import React, {memo, useEffect, useState} from 'react'
+import React, {memo} from 'react'
+import PropTypes from 'prop-types'
 
-var SVGComponent = function SVGComponent(_ref) {
-  var _ref$ssr = _ref.ssr
-  var ssr = _ref$ssr === void 0 ? false : _ref$ssr // eslint-disable-line
-  var className = _ref.className
+const SVG = ({className}) => (
+  <svg className={className} viewBox="0 0 24 24">
+    <path d="M16.8 9.2c.4-.4 1-.3 1.4.1.4.4.3 1-.1 1.4l-5 4.5c-.4.3-1 .3-1.3 0l-5-4.5c-.4-.4-.4-1-.1-1.4.4-.4 1-.4 1.4-.1l4.3 3.9 4.4-3.9z" />
+  </svg>
+)
 
-  var _useState = useState(ssr)
-  var render = _useState[0]
-  var setRender = _useState[1]
-
-  useEffect(
-    function() {
-      if (render === false) {
-        setRender(true)
-      }
-    },
-    [render, setRender]
-  )
-  return render === false
-    ? null
-    : React.createElement(
-        'svg',
-        {
-          className: className,
-          viewBox: '0 0 24 24'
-        },
-        React.createElement('path', {
-          d:
-            'M16.8 9.2c.4-.4 1-.3 1.4.1.4.4.3 1-.1 1.4l-5 4.5c-.4.3-1 .3-1.3 0l-5-4.5c-.4-.4-.4-1-.1-1.4.4-.4 1-.4 1.4-.1l4.3 3.9 4.4-3.9z'
-        })
-      )
+SVG.propTypes = {
+  className: PropTypes.string
 }
 
-export default memo(SVGComponent)
+export default memo(SVG)
