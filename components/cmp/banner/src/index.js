514de607d0b7e1e549ca5eec92cc60af06a4ad6f
midudev <miduga@gmail.com>
2020-01-31 17:16:32 +0100

    feat(cmp/banner): Use new @s-ui instead @schibstedspain

--- components/cmp/banner/src/index.js
+++ components/cmp/banner/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import CmpServices from '@schibstedspain/react-cmp-services'
+import CmpServices from '@s-ui/react-cmp-services'
 
 import CmpBannerContainer from './CmpBanner/index'
 
