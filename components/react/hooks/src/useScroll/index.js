aaf0ede0d1497605afd2cc670eabe28adfb69095
Joan León | @nucliweb <joan.leon@gmail.com>
2020-02-24 18:56:59 +0100

    feat(react/hooks): improve and optimize the code

--- components/react/hooks/src/useScroll/index.js
+++ components/react/hooks/src/useScroll/index.js
@@ -1,17 +1,19 @@
-import {useState, useEffect} from 'react'
+import {useState} from 'react'
+import useMount from '../useMount'
+
+const AXIS_MAP = {
+  Y: 'top',
+  X: 'left'
+}
+
+const isClient = typeof window !== 'undefined' && window.document
 
 export default function useScroll({axis = 'Y'} = {}) {
   const getBoundingClientRect = () =>
-    window?.document ? document.body.getBoundingClientRect() : 0
-
-  const AXIS_MAP = {
-    Y: 'top',
-    X: 'left'
-  }
+    isClient ? document.body.getBoundingClientRect() : 0
   const boundingKey = AXIS_MAP[axis]
 
   const [scroll, setScroll] = useState({
-    bodyOffset: getBoundingClientRect(),
     position: getBoundingClientRect()[boundingKey],
     scrollDirection: null
   })
@@ -21,18 +23,17 @@ export default function useScroll({axis = 'Y'} = {}) {
     const currentPosition = -currentBounding[boundingKey]
 
     setScroll(({position: prevPosition}) => ({
-      bodyOffset: currentBounding,
       position: currentPosition,
       scrollDirection: prevPosition > currentPosition ? 'DOWN' : 'UP'
     }))
   }
 
-  useEffect(() => {
+  useMount(() => {
     window.addEventListener('scroll', onScroll)
     return () => {
       window.removeEventListener('scroll', onScroll)
     }
-  }, []) // eslint-disable-line
+  }, [])
 
   return {
     position: Math.round(scroll.position),
