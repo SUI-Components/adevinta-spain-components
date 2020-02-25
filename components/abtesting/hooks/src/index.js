3389414afbfd2aeb7a9756ff45c9f00861977a8b
midudev <miduga@gmail.com>
2020-02-25 22:23:42 +0100

    feat(abtesting/hooks): Use latest @s-ui packages

--- components/abtesting/hooks/src/index.js
+++ components/abtesting/hooks/src/index.js
@@ -1,7 +1,7 @@
 export default function ReactHooks() {
   /* eslint-disable-next-line no-console */
   console.warn(
-    "[@schibstedspain/sui-abtesting-hooks] can't be used as a standalone package.\nYou must import a specific hook."
+    "[@s-ui/react-abtesting-hooks] can't be used as a standalone package.\nYou must import a specific hook."
   )
   return null
 }
