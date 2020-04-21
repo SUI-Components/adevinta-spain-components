b4aacf2ffd655f6202d969f44e1f16b610dafc15
maryam.malek <maryam.malek@adevinta.com>
2020-04-21 11:46:07 +0200

    docs(tcf/ui): remove playground and update demo to get styles

---
+++ demo/tcf/ui/demo/index.js
@@ -0,0 +1,13 @@
+import '../../../../components/tcf/firstLayer/src/index.scss'
+import TcfUi from '../../../../components/tcf/ui/src'
+import React from 'react'
+
+const TcfUiDemo = () => {
+  return (
+    <>
+      <TcfUi />
+    </>
+  )
+}
+
+export default TcfUiDemo
