ec626e740dfbff15f42493831a3501f5887f248a
maryam.malek <maryam.malek@adevinta.com>
2020-04-29 06:48:16 +0200

    docs(tcf/ui): remove unnecessary fragment

--- demo/tcf/ui/demo/index.js
+++ demo/tcf/ui/demo/index.js
@@ -3,11 +3,7 @@ import TcfUi from '../../../../components/tcf/ui/src'
 import React from 'react'
 
 const TcfUiDemo = () => {
-  return (
-    <>
-      <TcfUi />
-    </>
-  )
+  return <TcfUi />
 }
 
 export default TcfUiDemo
