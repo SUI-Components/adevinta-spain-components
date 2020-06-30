8ba0c34f31ab31dfa5304a696cac5a1d52e66daa
maryam.malek <maryam.malek@adevinta.com>
2020-06-30 16:37:14 +0200

    docs(tcf/secondLayer): improve demo for desktop/mobile change

--- demo/tcf/secondLayer/demo/index.js
+++ demo/tcf/secondLayer/demo/index.js
@@ -1,7 +1,9 @@
+import React, {useState} from 'react'
+
 import '../../../../components/tcf/secondLayer/src/index.scss'
+
 import TcfSecondLayer from '../../../../components/tcf/secondLayer/src'
 import {mockedUserConsent, mockedVendorList} from '../../utils/borosMock'
-import React from 'react'
 
 const TcfSecondLayerDemo = () => {
   const loadUserConsent = () => Promise.resolve(mockedUserConsent)
@@ -14,16 +16,38 @@ const TcfSecondLayerDemo = () => {
   const handleGoBack = () => {
     console.log('go back button pressed')
   }
+  const [isMobile, setIsMobile] = useState(false)
 
   return (
-    <TcfSecondLayer
-      isOpen
-      logo="https://frtassets.fotocasa.es/img/fotocasa_logo.svg"
-      loadUserConsent={loadUserConsent}
-      saveUserConsent={saveUserConsent}
-      getVendorList={getVendorList}
-      onGoBack={handleGoBack}
-    />
+    <>
+      <button
+        onClick={() => setIsMobile(!isMobile)}
+        style={{height: '25px', width: '100%', backgroundColor: '#2b91c1'}}
+      >
+        {isMobile ? 'Click to change to Desktop' : 'Click to change to Mobile'}
+      </button>
+      {isMobile && (
+        <TcfSecondLayer
+          isOpen
+          logo="https://frtassets.fotocasa.es/img/fotocasa_logo.svg"
+          loadUserConsent={loadUserConsent}
+          saveUserConsent={saveUserConsent}
+          getVendorList={getVendorList}
+          onGoBack={handleGoBack}
+          isMobile
+        />
+      )}
+      {!isMobile && (
+        <TcfSecondLayer
+          isOpen
+          logo="https://frtassets.fotocasa.es/img/fotocasa_logo.svg"
+          loadUserConsent={loadUserConsent}
+          saveUserConsent={saveUserConsent}
+          getVendorList={getVendorList}
+          onGoBack={handleGoBack}
+        />
+      )}
+    </>
   )
 }
 
