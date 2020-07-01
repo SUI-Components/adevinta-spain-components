d87b9c9b12e89bfa5b0aba19b94f4d1d60663992
maryam.malek <maryam.malek@adevinta.com>
2020-07-01 16:19:24 +0200

    docs(tcf/ui): add desktop/tablet/mobile in demo and necssary props

--- demo/tcf/ui/demo/index.js
+++ demo/tcf/ui/demo/index.js
@@ -1,9 +1,47 @@
 import '../../../../components/tcf/firstLayer/src/index.scss'
+import '../../../../components/tcf/secondLayer/src/index.scss'
 import TcfUi from '../../../../components/tcf/ui/src'
-import React from 'react'
+import React, {useState} from 'react'
 
 const TcfUiDemo = () => {
-  return <TcfUi />
+  const [isMobile, setIsMobile] = useState(false)
+  const [isTablet, setIsTablet] = useState(false)
+
+  // 3500px height added to be able to test the closing with scroll feature
+  return (
+    <div style={{height: '3500px'}}>
+      <button
+        onClick={() => {
+          setIsMobile(false)
+          setIsTablet(false)
+        }}
+      >
+        Desktop
+      </button>
+      <button
+        onClick={() => {
+          setIsMobile(true)
+          setIsTablet(false)
+        }}
+      >
+        Mobile
+      </button>
+      <button
+        onClick={() => {
+          setIsMobile(false)
+          setIsTablet(true)
+        }}
+      >
+        Tablet
+      </button>
+      <TcfUi
+        lang="es"
+        logo="https://frtassets.fotocasa.es/img/fotocasa_logo.svg"
+        isMobile={isMobile}
+        isTablet={isTablet}
+      />
+    </div>
+  )
 }
 
 export default TcfUiDemo
