cc3809f72a43d6d9f30c93e2b686cafab1f20528
Pablo Rey Sobral <pablo.rey@adevinta.com>
2020-07-09 11:11:34 +0200

    chore(examples): update borosMock for tcf/secondLayer demo

--- demo/tcf/utils/borosMock.js
+++ demo/tcf/utils/borosMock.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 const mockedEmptyUserConsent = {
   isNew: true,
-  specialFeature: {},
+  specialFeatures: {},
   purpose: {
     consents: {},
     legitimateInterests: {}
@@ -13,7 +13,7 @@ const mockedEmptyUserConsent = {
 }
 const mockedFullUserConsent = {
   isNew: false,
-  specialFeature: {1: true},
+  specialFeatures: {1: true},
   purpose: {
     consents: {
       1: false,
