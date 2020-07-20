ae9f4e87eaf60ec1165b3962612b76905929b924
Alex Castells <alextremp@hotmail.com>
2020-07-20 16:35:31 +0200

    refactor(tcf/ui): avoid eslint deps autofill on changes

--- components/tcf/ui/src/TCFContainer/TCFContainer.js
+++ components/tcf/ui/src/TCFContainer/TCFContainer.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, {Suspense, useState, useEffect} from 'react'
 import PropTypes from 'prop-types'
 import {useConsent} from '@s-ui/react-tcf-services'
