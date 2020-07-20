00704596d54a33c7e6c1dbf9768401a81c3362e7
midudev <miduga@gmail.com>
2020-07-20 11:59:43 +0200

    test(Root): skip tests not implemented

--- test/ad/blockerExtensionDetector/index.js
+++ test/ad/blockerExtensionDetector/index.js
@@ -12,7 +12,7 @@ import chaiDOM from 'chai-dom'
 
 chai.use(chaiDOM)
 
-describe('ad/blockerExtensionDetector', () => {
+describe.skip('ad/blockerExtensionDetector', () => {
   it('Render', () => {
     // Example TO BE DELETED!!!!
     // const {getByRole} = render(<AtomButton>HOLA</AtomButton>)
