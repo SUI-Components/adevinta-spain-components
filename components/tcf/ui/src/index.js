bf8b74230e4ba30bb5f663341772f96b3ca80787
Alex Castells <alextremp@hotmail.com>
2020-07-20 12:54:59 +0200

    fix(tcf/ui): provide default consent scope for adevinta

--- components/tcf/ui/src/index.js
+++ components/tcf/ui/src/index.js
@@ -3,8 +3,21 @@ import PropTypes from 'prop-types'
 import TCFContainer from './TCFContainer/TCFContainer'
 import ConsentProvider from '@s-ui/react-tcf-services'
 
+/*
+TECH-DEBT
+1. it should be passed from the outside
+2. if nothing is passed, it should use ALL vendor list data (done in services TcfRepository)
+ */
+const CONSENT_SCOPE = {
+  purposes: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+  specialPurposes: [1, 2],
+  features: [3],
+  specialFeatures: [1]
+}
+
 export default function TcfUi({
   lang,
+  scope = CONSENT_SCOPE,
   logo,
   isMobile,
   showVendors,
@@ -12,7 +25,7 @@ export default function TcfUi({
   showInModalForMobile = false
 }) {
   return (
-    <ConsentProvider language={lang} isMobile={isMobile}>
+    <ConsentProvider language={lang} isMobile={isMobile} scope={scope}>
       <TCFContainer
         logo={logo}
         showVendors={showVendors}
@@ -25,9 +38,10 @@ export default function TcfUi({
 
 TcfUi.displayName = 'TcfUi'
 TcfUi.propTypes = {
+  lang: PropTypes.string,
+  scope: PropTypes.object,
   isMobile: PropTypes.bool,
   showVendors: PropTypes.bool,
-  lang: PropTypes.string,
   logo: PropTypes.string,
   onCloseModal: PropTypes.func,
   showInModalForMobile: PropTypes.bool
