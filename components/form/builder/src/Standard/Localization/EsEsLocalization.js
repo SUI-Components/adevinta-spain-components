23dfed3d174b718c9349001c9b92f070cc9f4817
arnau-rius <arnau.rius.contact@gmail.com>
2020-07-27 13:47:17 +0200

    feat(form/builder): move to parent class methods

--- components/form/builder/src/Standard/Localization/EsEsLocalization.js
+++ components/form/builder/src/Standard/Localization/EsEsLocalization.js
@@ -2,11 +2,10 @@ import {Localization} from './Localization'
 
 export class EsEsLocalization extends Localization {
   fromStringToLocaleFloat() {
-    const removeAllDots = aString => aString.replace(/\./g, '')
-    const replaceFirstCommaWithDot = aString => aString.replace(',', '.')
-    const removeAllCommas = aString => aString.replace(/,/g, '')
     return parseFloat(
-      removeAllCommas(replaceFirstCommaWithDot(removeAllDots(this._value)))
+      this.removeAllCommas(
+        this.replaceFirstCommaWithDot(this.removeAllDots(this._value))
+      )
     )
   }
 }
--- components/form/builder/src/Standard/Localization/Localization.js
+++ components/form/builder/src/Standard/Localization/Localization.js
@@ -3,6 +3,18 @@ export class Localization {
     this._value = value
   }
 
+  removeAllDots(aString) {
+    return aString.replace(/\./g, '')
+  }
+
+  replaceFirstCommaWithDot(aString) {
+    return aString.replace(',', '.')
+  }
+
+  removeAllCommas(aString) {
+    return aString.replace(/,/g, '')
+  }
+
   fromStringToLocaleFloat() {
     throw new Error(
       '[Localization#fromStringToLocaleFloat] must be implemented'
