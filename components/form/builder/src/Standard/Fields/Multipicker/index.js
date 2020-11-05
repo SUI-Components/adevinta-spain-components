5f166b5c02f536d15ca3e8ba4db534fd59f5c626
arnau.riusr <arnau.riusr@adevinta.com>
2020-11-05 10:20:56 +0100

    fix(form/builder): typo

--- components/form/builder/src/Standard/Fields/Multipicker/index.js
+++ components/form/builder/src/Standard/Fields/Multipicker/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import Multipicker from '../../../Multipicker'
+import Multipicker from '../../../MultiPicker'
 
 const MultipickerField = ({
   field,
