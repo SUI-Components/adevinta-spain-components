0ead8be85ecd3b169b75e28083fbcb1ef7693080
midudev <miduga@gmail.com>
2020-11-23 12:04:57 +0100

    feat(form/checkbox): Use new jsx runtime

--- components/form/checkbox/src/index.js
+++ components/form/checkbox/src/index.js
@@ -1,5 +1,4 @@
 import PropTypes from 'prop-types'
-import React from 'react'
 import CheckboxChecked from '@s-ui/react-icons/lib/Checkboxchecked'
 import CheckboxUnchecked from '@s-ui/react-icons/lib/Checkboxunchecked'
 import cx from 'classnames'
