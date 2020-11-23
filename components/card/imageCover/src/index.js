855efbd16189f79cffd5d630f6e3b3308e20e8ce
midudev <miduga@gmail.com>
2020-11-23 12:04:55 +0100

    feat(card/imageCover): Use new jsx runtime

--- components/card/imageCover/src/index.js
+++ components/card/imageCover/src/index.js
@@ -1,6 +1,5 @@
 /* eslint-disable react/prop-types */
 import AtomPanel from '@s-ui/react-atom-panel'
-import React from 'react'
 import PropTypes from 'prop-types'
 import SuiTagChip from '@s-ui/react-tag-chip'
 
