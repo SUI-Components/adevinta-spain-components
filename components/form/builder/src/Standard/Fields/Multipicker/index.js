9b9e1c12a110c923374a2f5a8140e6cebcec838a
arnau.riusr <arnau.riusr@adevinta.com>
2020-11-24 09:05:30 +0100

    fix(form/builder): case sensitive import

--- components/form/builder/src/Standard/Fields/Multipicker/index.js
+++ components/form/builder/src/Standard/Fields/Multipicker/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import Multipicker from '../../../MultiPicker'
+import Multipicker from '../../../Multipicker'
 
 const MultipickerField = ({
   field,
