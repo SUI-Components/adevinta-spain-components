f4a773c5e8f42c00baab56c861af03614b157279
Pablo Rey Sobral <pablo.rey@adevinta.com>
2021-01-13 16:51:45 +0100

    feat(tcf/ui): save a full consent if it's a bot
    ISSUES CLOSED: PSP-3746

---
+++ components/tcf/ui/src/TCFContainer/isBotService.js
@@ -0,0 +1,18 @@
+const BOTS_USER_AGENTS = [
+  'googlebot',
+  'google-structured-data-testing-tool',
+  'bingbot',
+  'linkedinbot',
+  'mediapartners-google',
+  'apis-google',
+  'adsbot-google-mobile',
+  'adsbot-google',
+  'googlebot-image',
+  'googlebot-news',
+  'googlebot-video',
+  'adsbot-google-mobile-apps'
+]
+
+export default function isBotService({userAgent}) {
+  return BOTS_USER_AGENTS.some(ua => userAgent.toLowerCase().includes(ua))
+}
