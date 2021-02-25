c81d4e40f08f3d5ae8d78b6dcb49a5feb9159300
Rafael Moral Pareja <rafael.moral@adevinta.com>
2021-02-25 09:43:32 +0100

    chore(Root): fix jest test configuration

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  transform: {'.*': '<rootDir>/node_modules/babel-jest'},
   transformIgnorePatterns: [
     '<rootDir>/node_modules/(?!@s-ui/abtesting-toggle/lib/|@babel/runtime)'
   ]
