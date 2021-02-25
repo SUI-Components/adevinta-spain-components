9dbbe4c94ae3998cae2a883414f66ae32aacc419
Adria Velardos Palomar <adria.velardos@habitaclia.com>
2021-02-25 17:07:44 +0100

    test(Root): add initial commit test

---
+++ test/googleOneTap/loader/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import chai, {expect} from 'chai'
+import chaiDOM from 'chai-dom'
+import {render} from '@testing-library/react'
+
+chai.use(chaiDOM)
+
+describe('GoogleOneTapLoader', () => {
+  it('Render', () => {
+    render(<GoogleOneTapLoader />)
+    expect(true).to.be.eql(false)
+  })
+})
