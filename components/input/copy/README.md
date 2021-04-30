c3f16862b9b45418b87f0d24435657af226558c9
Sergi Quintela <sergi.quintela@adevinta.com>
2021-04-30 12:37:17 +0200

    feat(input/copy): add new component

---
+++ components/input/copy/README.md
@@ -0,0 +1,34 @@
+# InputCopy
+
+> Component that combines an input with a button to copy the value of the input.
+
+<!-- ![](./assets/preview.png) -->
+
+## Installation
+
+```sh
+$ npm install @s-ui/sui-input-copy
+```
+
+## Usage
+
+### Basic usage
+
+#### Import package and use the component
+
+```js
+import InputCopy from '@s-ui/sui-input-copy'
+
+return (<InputCopy />)
+```
+
+#### Import the styles (Sass)
+
+```css
+@import '~@s-ui/theme/lib/index';
+// @import 'your theme';
+@import '~@s-ui/sui-input-copy/lib/index';
+```
+
+
+> **Find full description and more examples in the [demo page](#).**
---
+++ components/input/copy/src/index.js
@@ -0,0 +1,69 @@
+import React, {useState} from 'react'
+import PropTypes from 'prop-types'
+
+import AtomButton from '@s-ui/react-atom-button'
+import AtomInput from '@s-ui/react-atom-input'
+import {useCopyToClipboard} from '@s-ui/react-hooks'
+
+const baseClass = 'sui-InputCopy'
+
+export default function InputCopy({
+  buttonProps = {},
+  buttonText = 'Copy',
+  buttonTextAfterCopied = 'Copied',
+  isCopied: isCopiedFromProps = false,
+  labelText,
+  onCopy,
+  textToCopy
+}) {
+  const [isCopied, setIsCopied] = useState(isCopiedFromProps)
+  const [, copyToClipboard] = useCopyToClipboard()
+
+  const handleCopy = event => {
+    const {onClick = () => {}} = buttonProps
+
+    copyToClipboard(textToCopy)
+    setIsCopied(true)
+
+    onClick(event)
+    onCopy(event)
+  }
+
+  const copyButtonProps = isCopied
+    ? {
+        children: buttonTextAfterCopied,
+        color: 'success',
+        design: 'outline',
+        ...buttonProps
+      }
+    : {
+        children: buttonText,
+        design: 'outline',
+        onClick: handleCopy,
+        ...buttonProps
+      }
+
+  return (
+    <div className={baseClass}>
+      {labelText && <label className={`${baseClass}-label`}>{labelText}</label>}
+      <AtomInput
+        button={
+          <AtomButton className={`${baseClass}-button`} {...copyButtonProps} />
+        }
+        disabled
+        value={textToCopy}
+      />
+    </div>
+  )
+}
+
+InputCopy.displayName = 'InputCopy'
+InputCopy.propTypes = {
+  buttonProps: PropTypes.object,
+  buttonText: PropTypes.string,
+  buttonTextAfterCopied: PropTypes.string,
+  isCopied: PropTypes.bool,
+  labelText: PropTypes.string,
+  onCopy: PropTypes.func,
+  textToCopy: PropTypes.string
+}
