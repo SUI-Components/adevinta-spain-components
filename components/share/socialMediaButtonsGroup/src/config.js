ddbb8a39813fe454fb35d4847eae5f4c225e68ba
Sergi Quintela <sergi.quintela@adevinta.com>
2021-05-03 16:12:37 +0200

    feat(share/socialMediaButtonsGroup): remove icons

--- components/share/socialMediaButtonsGroup/src/config.js
+++ components/share/socialMediaButtonsGroup/src/config.js
@@ -1,8 +1,3 @@
-import EmailIcon from '@adv-ui/fc-iconset/lib/EmailActionUnread'
-import WhatsAppIcon from '@adv-ui/fc-iconset/lib/MessagingWhatsapp'
-import FacebookIcon from '@adv-ui/fc-iconset/lib/SocialMediaFacebook'
-import TwitterIcon from '@adv-ui/fc-iconset/lib/SocialMediaTwitter'
-
 export const DEFAULT_SOCIAL_MEDIA_BUTTONS = [
   'mail',
   'whatsapp',
@@ -13,25 +8,21 @@ export const DEFAULT_SOCIAL_MEDIA_BUTTONS = [
 const DEFAULT_SOCIAL_MEDIA_DICTIONARY = {
   mail: {
     design: 'outline',
-    leftIcon: <EmailIcon />,
     literal: 'Email',
     url: 'mailto:?body='
   },
   whatsapp: {
     design: 'outline',
-    leftIcon: <WhatsAppIcon />,
     literal: 'WhatsApp',
     url: 'https://wa.me/?text='
   },
   facebook: {
     design: 'outline',
-    leftIcon: <FacebookIcon />,
     literal: 'Facebook',
     url: 'http://www.facebook.com/share.php?u='
   },
   twitter: {
     design: 'outline',
-    leftIcon: <TwitterIcon />,
     literal: 'Twitter',
     url: 'http://twitter.com/share?url='
   }
