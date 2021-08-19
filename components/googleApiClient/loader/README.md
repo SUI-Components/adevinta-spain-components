1be4cff2b668b692bf45c90b929469d7624d2a5c
Ivan Martinez Laborda <ivan.martinez@MacBook-MacBook-Pro-de-Ivan.local>
2021-08-19 13:05:46 +0200

    feat(googleApiClient/loader): add google-api-client-loader component

---
+++ components/googleApiClient/loader/README.md
@@ -0,0 +1,26 @@
+# GoogleApiClientLoader
+
+Ensures Google API Client Library script is loaded before allowing to render the child content
+
+<!-- ![](./assets/preview.png) -->
+
+## Installation
+
+```sh
+$ npm install @s-ui/sui-google-api-client-loader
+```
+
+## Usage
+[Google API Client Library for JavaScript documentation](https://github.com/google/google-api-javascript-client)
+
+#### Import package and use the component
+
+```js
+import GoogleApiClientLoader from '@s-ui/sui-google-api-client-loader'
+
+return (<GoogleOneTapLoader render={() => 'You can use Google API client library now!'}/>)
+```
+
+
+
+> **Find full description and more examples in the [demo page](#).**
---
+++ components/googleApiClient/loader/src/index.js
@@ -0,0 +1,34 @@
+/* eslint-disable camelcase */
+import PropTypes from 'prop-types'
+import ScriptLoader from '@s-ui/react-script-loader'
+
+const getGoogleApiClient = () => window && window.gapi
+const googleApiClientUrl = 'https://apis.google.com/js/client.js'
+
+export default function GoogleApiClientLoader({
+  render,
+  timeoutRender,
+  onTimeout = () => null,
+  detectionDelay = 15000
+}) {
+  return (
+    <ScriptLoader
+      src={googleApiClientUrl}
+      isAsync
+      verifier={getGoogleApiClient}
+      render={render}
+      timeoutRender={timeoutRender}
+      onTimeout={onTimeout}
+      detectionDelay={detectionDelay}
+    />
+  )
+}
+
+GoogleApiClientLoader.displayName = 'GoogleApiClientLoader'
+
+GoogleApiClientLoader.propTypes = {
+  render: PropTypes.func.isRequired,
+  timeoutRender: PropTypes.func,
+  onTimeout: PropTypes.func,
+  detectionDelay: PropTypes.number
+}
