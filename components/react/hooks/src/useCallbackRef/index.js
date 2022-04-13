516f0738d605af02b1bf538caa92ae88d54ca18d
Andrés Álvarez <andres.alvarez@Andress-MacBook-Pro.local>
2022-04-13 09:34:05 +0200

    feat(react/hooks): remove not needed deps

--- components/react/hooks/src/useCallbackRef/index.js
+++ components/react/hooks/src/useCallbackRef/index.js
@@ -2,13 +2,12 @@ import {useRef, useCallback} from 'react'
 
 import useIsomorphicLayoutEffect from '../useIsomorphicLayoutEffect'
 
-export default function useCallbackRef(fn, deps = []) {
+export default function useCallbackRef(fn) {
   const ref = useRef(fn)
 
   useIsomorphicLayoutEffect(() => {
     ref.current = fn
   })
 
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  return useCallback((...args) => ref.current?.(...args), deps)
+  return useCallback((...args) => ref.current?.(...args), [])
 }
