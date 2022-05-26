c82579316f81be1b6f8b9f181a46b3cc7bc54cd5
Sabrina Castelli <sabrinacastelli@live.com>
2022-05-26 08:37:15 +0200

    feat(components/form/builder): add an empty object as inputValues default value

--- components/form/builder/src/Range/Default/index.js
+++ components/form/builder/src/Range/Default/index.js
@@ -5,7 +5,7 @@ import {field, createComponentMemo} from '../../prop-types/index.js'
 
 import MoleculeInputField from '@s-ui/react-molecule-input-field'
 
-const joinInputValues = inputValues => {
+const joinInputValues = (inputValues = {}) => {
   const {from, to} = inputValues
   return [from, to].join(',')
 }
