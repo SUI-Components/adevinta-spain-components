94cb41c79a675e95629d5025f807a408c2eb337e
Alfredo Zimperz <alfredo.zimperz@adevinta.com>
2022-08-04 13:35:16 +0200

    refactor(components/spinner/basic): autofix file linting

--- components/spinner/basic/src/index.js
+++ components/spinner/basic/src/index.js
@@ -1,5 +1,5 @@
-import PropTypes from 'prop-types'
 import cx from 'classnames'
+import PropTypes from 'prop-types'
 
 export default function SpinnerBasic({size = 'medium'}) {
   const className = cx('sui-SpinnerBasic', `sui-SpinnerBasic--${size}`)
