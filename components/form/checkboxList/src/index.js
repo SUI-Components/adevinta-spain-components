b971e34871054ba3c6f557475f1aca91018adcda
Alfredo Zimperz <alfredo.zimperz@adevinta.com>
2022-08-04 13:35:10 +0200

    refactor(components/form/checkboxList): autofix file linting

--- components/form/checkboxList/src/index.js
+++ components/form/checkboxList/src/index.js
@@ -1,5 +1,5 @@
-import PropTypes from 'prop-types'
 import cx from 'classnames'
+import PropTypes from 'prop-types'
 
 const isChecked = ({selectedValues = [], value} = {}) => {
   return selectedValues.indexOf(value) !== -1
