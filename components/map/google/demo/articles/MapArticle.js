5ce52a61ed71ab1d06b8cc14ea22bac71ca1041c
Andrés Alvarez Sordo <a.alvarez.sor@gmail.com>
2022-08-23 15:06:39 +0200

    docs(components/map/google): improve documentation

--- components/map/google/demo/articles/MapArticle.js
+++ components/map/google/demo/articles/MapArticle.js
@@ -1,18 +1,53 @@
 import MapGoogle from 'components/map/google/src/index.js'
 import PropTypes from 'prop-types'
 
-import {Article, Cell, Grid, H2, Text} from '@s-ui/documentation-library'
+import {
+  Anchor,
+  Article,
+  Cell,
+  Code,
+  Grid,
+  H2,
+  H3,
+  Text
+} from '@s-ui/documentation-library'
 
 const MapArticle = ({apiKey, height, width}) => {
   return (
     <Article>
-      <H2>Static/Dynamic Map</H2>
-      <Text>Render a static/dynamic map</Text>
-      <Text>Click to switch from static to dynamic mode</Text>
-      <Grid cols={1} gutter={[8, 8]}>
-        <Cell></Cell>
-        <Cell style={{height, width}}>
+      <H2>Map</H2>
+      <Text>
+        Render a map. Toggle between a static map image and a dynamic map using{' '}
+        <Code>isInteractive</Code> prop. The dynamic map is a wrapper of{' '}
+        <Anchor href="https://react-google-maps-api-docs.netlify.app/#googlemap">
+          GoogleMap
+        </Anchor>{' '}
+        but also loads Google Maps script out of the box. Use{' '}
+        <Code>loaderNode</Code> and <Code>errorNode</Code> to optionally display
+        loading and error states. All the props defined in the wrapped component
+        are also available.
+      </Text>
+
+      <Grid cols={2} gutter={[8, 8]}>
+        <Cell>
+          <H3>Uncontrolled</H3>
+
+          <Text>Static by default but changes to dynamic when is clicked</Text>
+
           <MapGoogle apiKey={apiKey} height={height} width={width} />
+        </Cell>
+
+        <Cell>
+          <H3>Controlled</H3>
+
+          <Text>
+            Use <Code>isInteractive</Code> prop to toggle between static and
+            dynamic. In this will be always interactive
+          </Text>
+
+          <div style={{height, width}}>
+            <MapGoogle apiKey={apiKey} isInteractive />
+          </div>
         </Cell>
       </Grid>
     </Article>
--- components/map/google/demo/index.js
+++ components/map/google/demo/index.js
@@ -2,8 +2,8 @@ import {useState} from 'react'
 
 import {Box, H1, Input} from '@s-ui/documentation-library'
 
+import MapAddOnsArticle from './articles/MapAddOnsArticle.js'
 import MapArticle from './articles/MapArticle.js'
-import MapDynamicArticle from './articles/MapDynamicArticle.js'
 import MapImageArticle from './articles/MapImageArticle.js'
 
 const className = 'DemoMapGoogle'
@@ -32,12 +32,10 @@ export default () => {
         height={600}
         width={600}
       />
-      <MapDynamicArticle
-        apiKey={apiKey}
-        key={`${apiKey}-MapDynamicArticle`}
-        isInteractive
-      />
+
       <MapImageArticle apiKey={apiKey} height={600} width={600} />
+
+      <MapAddOnsArticle apiKey={apiKey} key={`${apiKey}-MapAddOnsArticle`} />
     </div>
   )
 }
