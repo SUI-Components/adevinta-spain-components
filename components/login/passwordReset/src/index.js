2553d6245ed557169e5aa379b5b3013e75122600
Oriol Egea <egea@oriol.im>
2023-01-18 09:41:05 +0100

    feat(components/login/passwordReset): Create scaffolding for passwordReset component

---
+++ components/login/passwordReset/src/index.js
@@ -0,0 +1,12 @@
+// import PropTypes from 'prop-types'
+
+export default function LoginPasswordReset() {
+  return (
+    <div className="sui-LoginPasswordReset">
+      <h1>LoginPasswordReset</h1>
+    </div>
+  )
+}
+
+LoginPasswordReset.displayName = 'LoginPasswordReset'
+LoginPasswordReset.propTypes = {}
