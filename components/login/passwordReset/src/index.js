7d1206af973a43e244c2a83920b4c89a876cf966
David Martin Cerda <david.martin@scmspain.com>
2023-01-19 12:47:55 +0100

    feat(components/login/passwordReset): Init html structure

--- components/login/passwordReset/src/index.js
+++ components/login/passwordReset/src/index.js
@@ -2,14 +2,20 @@ import PropTypes from 'prop-types'
 
 import FormHandler from './components/FormHandler.js'
 import StageInfo from './components/StageInfo.js'
+import {BASE_CLASS} from './config.js'
 import {PasswordResetProvider} from './context.js'
 
 export default function LoginPasswordReset({i18n: customI18n}) {
   return (
     <PasswordResetProvider customI18n={customI18n}>
-      <div className="sui-LoginPasswordReset">
-        <StageInfo />
-        <FormHandler />
+      <div className={BASE_CLASS}>
+        <div className={`${BASE_CLASS}-item`}>
+          <StageInfo />
+        </div>
+        <div className={`${BASE_CLASS}-item`}>
+          <FormHandler />
+        </div>
+        <div className={`${BASE_CLASS}-item ${BASE_CLASS}-itemFalse`} />
       </div>
     </PasswordResetProvider>
   )
