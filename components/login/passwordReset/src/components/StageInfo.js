281f9f40aa2d3ff22b93a531ee7e40cd4f803715
David Martin Cerda <david.martin@scmspain.com>
2023-01-23 13:00:54 +0100

    feat(components/login/passwordReset): add stepper to component

--- components/login/passwordReset/src/components/StageInfo.js
+++ components/login/passwordReset/src/components/StageInfo.js
@@ -1,5 +1,30 @@
+import MoleculeProgressSteps, {
+  MoleculeProgressStep,
+  STATUSES
+} from '@s-ui/react-molecule-progress-steps'
+
+import {BASE_CLASS} from './../config.js'
 const StageInfo = () => {
-  return <div className="sui-LoginPasswordReset-stageInfo">StageInfo</div>
+  return (
+    <div className={`${BASE_CLASS}-stageInfo`}>
+      <div className={`${BASE_CLASS}-stageInfoProgress`}>
+        <MoleculeProgressSteps compressed>
+          <MoleculeProgressStep
+            label="Introduce tu email"
+            status={STATUSES.ACTIVE}
+          />
+          <MoleculeProgressStep />
+        </MoleculeProgressSteps>
+      </div>
+      <h1 className={`${BASE_CLASS}-stageInfoTitle`}>
+        ¿Has olvidado tu contraseña?
+      </h1>
+      <p className={`${BASE_CLASS}-stageInfoMessage`}>
+        Escribe tu email de usuario y te enviaremos un correo para poder
+        restaurarla. Recuerda que solo es válido durante 24h.
+      </p>
+    </div>
+  )
 }
 
 StageInfo.displayName = 'StageInfo'
