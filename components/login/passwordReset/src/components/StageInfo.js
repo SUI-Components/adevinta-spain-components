fa8e662a02a7b216d7a959ff5d0fc93b8a6b81ec
David Martin Cerda <david.martin@scmspain.com>
2023-01-23 14:31:59 +0100

    feat(components/login/passwordReset): add literals

--- components/login/passwordReset/src/components/StageInfo.js
+++ components/login/passwordReset/src/components/StageInfo.js
@@ -4,24 +4,25 @@ import MoleculeProgressSteps, {
 } from '@s-ui/react-molecule-progress-steps'
 
 import {BASE_CLASS} from './../config.js'
+import useI18n from './../hooks/useI18n.js'
 const StageInfo = () => {
+  const i18n = useI18n()
   return (
     <div className={`${BASE_CLASS}-stageInfo`}>
       <div className={`${BASE_CLASS}-stageInfoProgress`}>
         <MoleculeProgressSteps compressed>
           <MoleculeProgressStep
-            label="Introduce tu email"
+            label={i18n.t('LOGIN_CROSS.PASSWORD_RESET.STEP_1.LABEL')}
             status={STATUSES.ACTIVE}
           />
           <MoleculeProgressStep />
         </MoleculeProgressSteps>
       </div>
       <h1 className={`${BASE_CLASS}-stageInfoTitle`}>
-        ¿Has olvidado tu contraseña?
+        {i18n.t('LOGIN_CROSS.PASSWORD_RESET.STEP_1.TITLE')}
       </h1>
       <p className={`${BASE_CLASS}-stageInfoMessage`}>
-        Escribe tu email de usuario y te enviaremos un correo para poder
-        restaurarla. Recuerda que solo es válido durante 24h.
+        {i18n.t('LOGIN_CROSS.PASSWORD_RESET.STEP_1.MESSAGE')}
       </p>
     </div>
   )
--- components/login/passwordReset/src/literals/es-ES.js
+++ components/login/passwordReset/src/literals/es-ES.js
@@ -1,1 +1,18 @@
-export default {}
+export default {
+  LOGIN_CROSS: {
+    PASSWORD_RESET: {
+      STEP_1: {
+        LABEL: 'Introduce tu email',
+        MESSAGE:
+          'Escribe tu email de usuario y te enviaremos un correo para poder restaurarla. Recuerda que solo es válido durante 24h.',
+        TITLE: '¿Has olvidado tu contraseña?'
+      },
+      STEP_2: {
+        LABEL: 'Crea tu nueva contraseña',
+        MESSAGE:
+          'Recuerda que para que la contraseña sea válida esta debe contener un mínimo de 8 letras o números.',
+        TITLE: '¿Has olvidado tu contraseña?'
+      }
+    }
+  }
+}
