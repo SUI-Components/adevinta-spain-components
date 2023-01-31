2b5007d21fe2b97d1a2d3b247a59e3bf21e899e5
Oriol Egea <egea@oriol.im>
2023-01-31 15:53:23 +0100

    refactor(components/login/passwordReset): Move password input logic to a specific component

--- components/login/passwordReset/src/components/Form/PasswordChangeForm.js
+++ components/login/passwordReset/src/components/Form/PasswordChangeForm.js
@@ -5,22 +5,14 @@ import AtomButton, {
   atomButtonShapes,
   atomButtonSizes
 } from '@s-ui/react-atom-button'
-import MoleculeInputField from '@s-ui/react-molecule-input-field'
 
-import {
-  BASE_CLASS,
-  DEFAULT_EYE_CLOSED_OUTLINE,
-  DEFAULT_EYE_OPEN_OUTLINE
-} from './../../config.js'
+import {BASE_CLASS} from './../../config.js'
 import useI18n from './../../hooks/useI18n.js'
+import PasswordInputField from './PasswordInputField.js'
 const PasswordChangeForm = () => {
   const i18n = useI18n()
   const [newPassword, setNewPassword] = useState('')
-  const [typeNewPasswordField, setTypeNewPasswordField] =
-    useState('sui-password')
   const [repeatPassword, setRepeatPassword] = useState('')
-  const [typeRepeatPasswordField, setTypeRepeatPasswordField] =
-    useState('sui-password')
 
   const handleChange = e => {
     const {value} = e?.target
@@ -40,32 +32,18 @@ const PasswordChangeForm = () => {
   return (
     <>
       <div className={`${BASE_CLASS}-formInput`}>
-        <MoleculeInputField
+        <PasswordInputField
           // errorText={errorText}
           id="new_password"
           label={i18n.t('LOGIN_CROSS.PASSWORD_RESET.STEP_2.NEW_PASSWORD_LABEL')}
           name="new_password"
           onChange={handleChange}
           placeholder={i18n.t('LOGIN_CROSS.PASSWORD_RESET.STEP_2.PLACEHOLDER')}
-          size="l"
-          type={typeNewPasswordField}
-          rightIcon={
-            typeNewPasswordField === 'sui-password'
-              ? DEFAULT_EYE_CLOSED_OUTLINE
-              : DEFAULT_EYE_OPEN_OUTLINE
-          }
-          onClickRightIcon={() =>
-            typeNewPasswordField === 'sui-password'
-              ? setTypeNewPasswordField('text')
-              : setTypeNewPasswordField('sui-password')
-          }
-          pwHideLabel=""
-          pwShowLabel=""
           value={newPassword}
         />
       </div>
       <div className={`${BASE_CLASS}-formInput`}>
-        <MoleculeInputField
+        <PasswordInputField
           // errorText={errorText}
           id="repeat_password"
           label={i18n.t(
@@ -74,20 +52,6 @@ const PasswordChangeForm = () => {
           name="repeat_password"
           onChange={handleChangeRepeat}
           placeholder={i18n.t('LOGIN_CROSS.PASSWORD_RESET.STEP_2.PLACEHOLDER')}
-          size="l"
-          type={typeRepeatPasswordField}
-          rightIcon={
-            typeRepeatPasswordField === 'sui-password'
-              ? DEFAULT_EYE_CLOSED_OUTLINE
-              : DEFAULT_EYE_OPEN_OUTLINE
-          }
-          onClickRightIcon={() =>
-            typeRepeatPasswordField === 'sui-password'
-              ? setTypeRepeatPasswordField('text')
-              : setTypeRepeatPasswordField('sui-password')
-          }
-          pwHideLabel=""
-          pwShowLabel=""
           value={repeatPassword}
         />
       </div>
