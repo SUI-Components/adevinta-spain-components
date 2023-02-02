b4475c0afe7f96ce2f8bf5d29fe19da2c9f35920
Oriol Egea <egea@oriol.im>
2023-02-02 17:32:07 +0100

    feat(components/login/passwordReset): Move PasswordResetForm state management to a custom hook using

---
+++ components/login/passwordReset/src/hooks/components/usePasswordResetFormState.js
@@ -0,0 +1,93 @@
+import {useReducer} from 'react'
+
+const usePasswordResetFormState = () => {
+  const initialState = {
+    email: '',
+    isLoading: false,
+    errorText: '',
+    notification: {
+      text: '',
+      isError: false
+    }
+  }
+
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case 'SET_EMAIL':
+        return {
+          ...state,
+          email: action.payload,
+          isLoading: false
+        }
+
+      case 'SET_NOTIFICATION':
+        return {
+          ...state,
+          notification: {
+            text: action.payload.text,
+            isError: action.payload.isError
+          },
+          isLoading: false
+        }
+
+      case 'SET_LOADING':
+        return {
+          ...state,
+          isLoading: action.payload
+        }
+
+      case 'SET_ERROR_TEXT':
+        return {
+          ...state,
+          errorText: action.payload
+        }
+
+      default:
+        return state
+    }
+  }
+
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  // Action for each of the above
+  const setEmail = email => {
+    dispatch({
+      type: 'SET_EMAIL',
+      payload: email
+    })
+  }
+
+  const setNotification = ({text, isError}) => {
+    dispatch({
+      type: 'SET_NOTIFICATION',
+      payload: {
+        text,
+        isError
+      }
+    })
+  }
+
+  const setIsLoading = isLoading => {
+    dispatch({
+      type: 'SET_LOADING',
+      payload: isLoading
+    })
+  }
+
+  const setErrorText = errorText => {
+    dispatch({
+      type: 'SET_ERROR_TEXT',
+      payload: errorText
+    })
+  }
+
+  return {
+    state,
+    setEmail,
+    setNotification,
+    setIsLoading,
+    setErrorText
+  }
+}
+
+export default usePasswordResetFormState
