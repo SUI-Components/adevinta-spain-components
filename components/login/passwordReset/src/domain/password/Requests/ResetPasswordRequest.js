a4979f0ade543adb15326f4f809e96b404df9cf4
Oriol Egea <egea@oriol.im>
2023-02-06 18:20:46 +0100

    feat(components/login/passwordReset): Avoid linter problems

--- components/login/passwordReset/src/domain/password/Requests/ResetPasswordRequest.js
+++ components/login/passwordReset/src/domain/password/Requests/ResetPasswordRequest.js
@@ -15,7 +15,7 @@ export class ResetPasswordRequest extends ValueObject {
     }
 
     const emailRegex =
-      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      /(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))/
     const isValidEmail = emailRegex.test(email)
 
     if (!isValidEmail) {
