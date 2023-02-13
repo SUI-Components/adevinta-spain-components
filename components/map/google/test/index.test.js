f34d6c58c8015b2d034226ea8f62314b1d04175e
Lola Rufino <lola.rufino@adevinta.com>
2023-02-13 14:09:31 +0100

    test(components/map/google): Delete it only from test

--- components/map/google/test/index.test.js
+++ components/map/google/test/index.test.js
@@ -147,7 +147,7 @@ describe('MapGoogleImage', () => {
     expect(screen.getByRole('img', {name: props.alt})).to.have.attr('src')
   })
 
-  it.only('should render default custom image when passing a signed url prop', async () => {
+  it('should render default custom image when passing a signed url prop', async () => {
     // Given
     const props = {
       ...REQUIRED_MAPGOOGLEIMAGE_PROPS,
