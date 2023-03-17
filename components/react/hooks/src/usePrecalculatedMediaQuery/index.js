9ccb6953a16a576e0ca12a215725671d5c59d651
xavi-murcia <xavi.murcia@adevinta.com>
2023-03-17 13:13:02 +0100

    feat(components/react/hooks): create usePrecalculatedMediaQuery to avoid CLS in some scenarios

---
+++ components/react/hooks/src/usePrecalculatedMediaQuery/index.js
@@ -0,0 +1,19 @@
+import useMediaQuery from '../useMediaQuery/index.js'
+
+const usePreCalculatedMediaQuery = queryInput => {
+  const supportMatchMedia =
+    typeof window !== 'undefined' && typeof window.matchMedia !== 'undefined'
+  const query = queryInput.replace(/^@media( ?)/m, '')
+  const matchMedia = supportMatchMedia ? window.matchMedia : null
+
+  let matches = true
+
+  if (supportMatchMedia) {
+    const queryList = matchMedia(query)
+    matches = queryList.matches
+  }
+
+  return useMediaQuery(queryInput, {defaultMatches: matches})
+}
+
+export default usePreCalculatedMediaQuery
