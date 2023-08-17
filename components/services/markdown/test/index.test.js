5ac94ba61909d7ae984b4acd665100d37cb0580e
Xavier Conejo <xavier.conejo@adevinta.com>
2023-08-17 11:54:01 +0200

    feat(components/services/markdown): update test

--- components/services/markdown/test/index.test.js
+++ components/services/markdown/test/index.test.js
@@ -7,13 +7,13 @@ import ServiceMarkdown from '../src/index.js'
 
 chai.use(chaiDOM)
 
-describe('services/markdown', async () => {
+describe('services/markdown', () => {
   it('Render', () => {
-    const {findByText} = render(
+    const {getByText} = render(
       <ServiceMarkdown src="https://sui-statics-frontend.spain.advgo.net/texts/fotocasa-pro/es/cookies.md" />
     )
 
-    const text = await findByText('Política de cookies de Fotocasa Pro')
+    const text = getByText('Política de cookies de Fotocasa Pro')
     expect(text).not.to.be.null
   })
 })
