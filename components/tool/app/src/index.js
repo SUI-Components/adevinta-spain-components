a5c6d0860cc4a67f8cefbe864cd1185988228733
Oriol Egea <egea@oriol.im>
2023-11-13 11:42:05 +0100

    feat(components/tool/app): Initial commit for sui-apps

---
+++ components/tool/app/src/index.js
@@ -0,0 +1,12 @@
+// import PropTypes from 'prop-types'
+
+export default function ToolApp() {
+  return (
+    <div className="sui-ToolApp">
+      <h1>ToolApp</h1>
+    </div>
+  )
+}
+
+ToolApp.displayName = 'ToolApp'
+ToolApp.propTypes = {}
