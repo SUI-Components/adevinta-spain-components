0157bbc3b618af61cf7f6a783dd9dc55976a1ad7
jordi.munoz@adevinta.com <jordi.munoz@adevinta.com>
2023-12-03 21:06:51 +0100

    chore(components/cookie/banner): lint js files

--- components/cookie/banner/src/index.js
+++ components/cookie/banner/src/index.js
@@ -12,29 +12,20 @@ const COOKIE_TTL = 31557600000
 const NO_OP = () => {}
 const EmptyIcon = () => null
 
-function CookieBanner({
-  cookieKey = 'user-has-accepted-cookies',
-  lang,
-  link,
-  onAccept = NO_OP
-}) {
+function CookieBanner({cookieKey = 'user-has-accepted-cookies', lang, link, onAccept = NO_OP}) {
   const [hasAcceptedCookies, setHasAcceptedCookies] = useState(true)
 
   const i18n = I18N[lang]
 
   function getHasAcceptedCookie() {
-    return decodeURIComponent(
-      (`; ${document.cookie}`.split(`; ${cookieKey}=`)[1] || '').split(';')[0]
-    )
+    return decodeURIComponent((`; ${document.cookie}`.split(`; ${cookieKey}=`)[1] || '').split(';')[0])
   }
 
   function setCookie() {
     // save the cookie with a true value
     const value = `${cookieKey}=true`
     // set the expiration date for a better browser support (IE11 has problems with max-age)
-    const expires = `expires=${new Date(
-      +new Date() + COOKIE_TTL
-    ).toUTCString()}`
+    const expires = `expires=${new Date(+new Date() + COOKIE_TTL).toUTCString()}`
     // Set the cookie with the value and the expiration date
     document.cookie = `${value};${expires}`
   }
@@ -67,11 +58,9 @@ function CookieBanner({
           <div>
             <h3 className={`${CLASS}-title`}>{i18n.TITLE}</h3>
             <span className={`${CLASS}-text`}>
-              Utilizamos cookies propias y de terceros, y tecnología similar,
-              para recordar tus preferencias, elaborar estadísticas, crear
-              perfiles publicitarios, mostrarte publicidad y contenido
-              personalizado, por ejemplo analizando tu navegación. Si continúas
-              navegando o haces clic en el botón "Seguir navegando", aceptas su
+              Utilizamos cookies propias y de terceros, y tecnología similar, para recordar tus preferencias, elaborar
+              estadísticas, crear perfiles publicitarios, mostrarte publicidad y contenido personalizado, por ejemplo
+              analizando tu navegación. Si continúas navegando o haces clic en el botón "Seguir navegando", aceptas su
               uso. Puedes revocar el consentimiento en{' '}
               <a className="sui-CmpUi-link" href={link}>
                 nuestra política de cookies
