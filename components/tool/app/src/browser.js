f24b5dd833514c30f12145a7e43444ed4f0e3a05
Oriol Egea <egea@oriol.im>
2024-07-09 11:31:31 +0200

    feat(components/tool/app): Add event listener functions to in-app browser

--- components/tool/app/src/browser.js
+++ components/tool/app/src/browser.js
@@ -2,3 +2,5 @@ import {Browser} from '@capacitor/browser'
 
 export const close = () => Browser.close()
 export const open = config => Browser.open(config)
+export const addListener = (eventName, listener) => Browser.addListener(eventName, listener)
+export const removeAllListeners = () => Browser.removeAllListeners()
