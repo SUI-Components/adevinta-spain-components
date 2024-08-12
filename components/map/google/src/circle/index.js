db6781cf2a8384794cc11250ca901fc0177c0a0b
andresin87 <andresin87@gmail.com>
2024-08-12 11:05:05 +0200

    chore(Root): tokens css native

--- components/map/google/src/circle/index.js
+++
@@ -1,9 +0,0 @@
-import {Circle} from '@react-google-maps/api'
-
-function MapGoogleCircle(props) {
-  return <Circle {...props} />
-}
-
-MapGoogleCircle.displayName = 'MapGoogleCircle'
-
-export default MapGoogleCircle
--- components/map/google/src/polygon/index.js
+++
@@ -1,9 +0,0 @@
-import {Polygon} from '@react-google-maps/api'
-
-function MapGooglePolygon(props) {
-  return <Polygon {...props} />
-}
-
-MapGooglePolygon.displayName = 'MapGooglePolygon'
-
-export default MapGooglePolygon
